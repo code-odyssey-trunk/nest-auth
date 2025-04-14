e0a122251267ab30e2bb4fd383c65dcb28929232
code-odyssey-trunk <jablog2020@gmail.com>
2025-04-14 11:02:18 +0200

    Initial commit with Create User + Password, Login Progress

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { Response } from 'express';
+import { User } from 'src/users/schema/user.schema';
+import { UsersService } from 'src/users/users.service';
+import { TokenPayload } from './token-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const tokenPayload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const expiresAccessToken = new Date();
+    expiresAccessToken.setMilliseconds(
+      expiresAccessToken.getTime() +
+        parseInt(
+          this.configService.getOrThrow<string>(
+            'JWT_ACCESS_TOKEN_EXPIRATION_MS',
+          ),
+        ),
+    );
+  }
+
+  async verifyUser(email: string, password: string) {
+    try {
+      const user = await this.userService.getUser({ email });
+      const authenticated = await compare(password, user.password);
+      if (!authenticated) {
+        throw new UnauthorizedException();
+      }
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { FilterQuery, Model } from 'mongoose';
+import { CreateUserRequest } from './dto/create-user.request';
+import { hash } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async create(data: CreateUserRequest) {
+    await new this.userModel({
+      ...data,
+      password: await hash(data.password, 10),
+    }).save();
+  }
+
+  async getUser(query: FilterQuery<User>) {
+    const user = (await this.userModel.findOne(query)).toObject();
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+}
